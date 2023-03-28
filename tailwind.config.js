9b60576b60e46cc6eb83ad0c1a089115c2599825
Hiren Savaliya <hirend@hhgsoftech.com>
2023-03-28 19:16:18 +0530

    one page done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./**/*.html"],
   theme: {
+    screens: {
+      sm: '576px',
+      md: '768px',
+      lg: '992px',
+      xl: '1142px',
+    },
+    container: {
+      center: true,
+      padding: "1rem",
+    },
+    fontFamily: {
+      dmsans: ["DM Sans", "sans-serif"],
+    },
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      white: '#ffffff',
+      black: '#12022F',
+      indigo: '#594D6D',
+      yellow: '#FFDDA9',
+      gray: '#E7E5EA',
+      violet: '#391484',
+    },
     extend: {},
   },
   plugins: [],
